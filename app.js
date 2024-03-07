234e5174d326db5733bc68934ec0a96abd3e4d93
BoDee Angus <bodee.angus@gmail.com>
2024-03-06 18:43:42 -0700

    There was an error with my relative path logic, fixed now

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 import fs from 'fs/promises';
 
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const packagePath = path.join(__dirname, './package.json');
+const appPath = path.join(__dirname, './defaultApp.js');
+const appTesterPath = path.join(__dirname, './defaultAppTester.js');
+
 export default async function (packageName, packageDesc, gitHubUrl) {
   const shortenedUrl = gitHubUrl.replace('.git', '');
   const bugsUrl = shortenedUrl + '/issues';
   const homepageUrl = shortenedUrl + '#readme';
   try {
-    const packageFile = await fs.readFile('./package.json', 'utf8');
+    const packageFile = await fs.readFile(packagePath, 'utf8');
     const packageData = JSON.parse(packageFile);
     packageData.name = packageName;
     packageData.description = packageDesc;
@@ -14,11 +23,17 @@ export default async function (packageName, packageDesc, gitHubUrl) {
     packageData.homepage = homepageUrl;
     packageData.dependencies = {};
 
+    const appFile = await fs.readFile(appPath, 'utf8');
+    const appTesterFile = await fs.readFile(appTesterPath, 'utf8');
+
     await fs.writeFile(
       `../${packageName}/package.json`,
       JSON.stringify(packageData, null, 2),
       'utf8'
     );
+
+    await fs.writeFile(`../${packageName}/app.js`, appFile, 'utf8');
+    await fs.writeFile(`../${packageName}/appTester.js`, appTesterFile, 'utf8');
 
     console.log(
       'Package_Configurer has successfully configured the package.json file for:'
